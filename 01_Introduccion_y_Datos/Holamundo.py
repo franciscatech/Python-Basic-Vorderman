23171bd3b04e93f1a69071e512484aea20560053
Francisca Rosa <frs0990@gmail.com>
2025-11-19 13:23:56 +0100

    Update Holamundo.py
    fix: Eliminado texto erróneo de la línea 1 y corregido el comentario.

--- 01_Introduccion_y_Datos/Holamundo.py
+++ 01_Introduccion_y_Datos/Holamundo.py
@@ -1,2 +1,4 @@
-#Primer paso, básico, que todos los programadores y programadoras debemos aprender. 
+# Este es el primer ejercicio de salida de texto (Hola mundo)
 print("¡Hola, mundo!")
+
+fix: Eliminado texto erróneo de la línea 1 y corregido el comentario.
