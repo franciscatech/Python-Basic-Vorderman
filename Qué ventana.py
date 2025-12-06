1b5a49aa50f7a4d496c4b0a8ab27835ef3bdfae9
Francisca Rosa <frs0990@gmail.com>
2025-12-06 19:02:46 +0100

    Update Qué ventana.py
    Ejercicios de Vorderman

--- Qué ventana.py
+++ Qué ventana.py
@@ -14,3 +14,5 @@ from turtle import *
 forward(100)
 right(120)
 forward(100)
+
+#Pequeño código de Python con algunos ejercicios básicos de libro de Vorderman. Las lineas 6 a 11 son investigación propia basándas en las anteriores. 
