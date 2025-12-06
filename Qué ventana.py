64eae1672264884ead6ea008059d03438269b1e7
Francisca Rosa <frs0990@gmail.com>
2025-12-06 18:58:38 +0100

    Qué ventana
    Un nuevo ejercicio de libro de Vorderman

---
+++ Qué ventana.py
@@ -0,0 +1,16 @@
+a = 10
+b = 4
+print(a + b)
+print(a - b)
+
+a = 255
+b = 125
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+from turtle import *
+forward(100)
+right(120)
+forward(100)
