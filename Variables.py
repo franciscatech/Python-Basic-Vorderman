233c1b5cf41feb99eacbdde49903dc7cfe2bd691
Francisca Rosa <frs0990@gmail.com>
2025-12-07 16:55:14 +0100

    Variables
    Breve demostración de las variables en Python.

---
+++ Variables.py
@@ -0,0 +1,26 @@
+huesos = 3
+nombre_del_perro = "Bruno"
+print(huesos)
+print(nombre_del_perro)
+regalos = 2
+print(regalos)
+regalos = 3
+print (regalos)
+conejos = 5
+sombreros = conejos
+print(conejos, sombreros)
+conejos = 10
+print(conejos, sombreros)
+
+#Hasta aquí, hemos seguido las indicaciones del ejercicio. Ahora, vamos a "picar" por nuestra cuenta.
+
+chuches = 12
+nombre_del_gato = "Chonflus"
+amigos = 2
+juguetes = 5
+print(chuches, nombre_del_gato, amigos, juguetes)
+hermanos = juguetes
+print(chuches, nombre_del_gato, amigos, juguetes, hermanos)
+chuches = 9
+juguetes = 3
+print(chuches, nombre_del_gato, amigos, juguetes, hermanos)
