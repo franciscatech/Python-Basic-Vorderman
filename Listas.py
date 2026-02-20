1d9864edcc952ff49300fe739d72eb3b03c570ed
Francisca Rosa <frs0990@gmail.com>
2026-02-20 13:10:18 +0100

    Listas en Python
    Ejercicios del libro Introducción a la informática.

---
+++ Listas.py
@@ -0,0 +1,15 @@
+milista = ["manzana", "leche", "queso", "helado", "limonada", "té"]
+nombres = ["Susi", "Marta", "Iván"]
+for item in nombres:
+    print("Hola", item)
+    
+x = [1, 2, 3, 4]
+y = [5, 6, 7, 8]
+z = x + y
+print (z)
+
+maleta = [["gorra", "corbata", "calcetín"],["bolso", "zapato", "camisa"]]
+print(maleta)
+print(maleta[1])
+print(maleta[1] [2])
+
